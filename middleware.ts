1d94b04ff2aa9f3bd2c97f9b416fb98f97324153
MinSeobKim <icdmiracle927@gmail.com>
2023-12-20 21:21:38 +0900

    Feat : url 기능 변경

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getCurrentUrl } from './utils/get-current-url';
 
 export const middleware = (req: NextRequest) => {
   // if req.nextUrl.pathname doesn't start with /api, return
   if (!req.nextUrl.pathname.startsWith('/api')) return;
 
-  const url =
-    process.env.NODE_ENV === 'development'
-      ? process.env.QUERY_PATH
-      : getCurrentUrl();
+  const url = process.env.QUERY_PATH;
   return NextResponse.rewrite(`${url}/api/bypass/get`);
 };
 
