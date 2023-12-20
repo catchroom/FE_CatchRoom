de4ca455915c3494c72bb370c46412878a0d65ac
MinSeobKim <icdmiracle927@gmail.com>
2023-12-20 20:58:06 +0900

    Feat : middleware api route 우회 기능 추가

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,14 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getCurrentUrl } from './utils/get-current-url';
 
 export const middleware = (req: NextRequest) => {
-  const url = getCurrentUrl();
-  console.log('url', req.nextUrl.pathname);
-  return NextResponse.rewrite(`${url}/api/v1/get`);
+  // if req.nextUrl.pathname doesn't start with /api, return
+  if (!req.nextUrl.pathname.startsWith('/api')) return;
+
+  const url =
+    process.env.NODE_ENV === 'development'
+      ? process.env.QUERY_PATH
+      : getCurrentUrl();
+  return NextResponse.rewrite(`${url}/api/bypass/get`);
 };
 
 export const config = {
