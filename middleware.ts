5f5ff6da5fbdb3d5994848c45563148c64869dd2
MinSeobKim <icdmiracle927@gmail.com>
2023-12-20 21:30:54 +0900

    test : 주소 삭제 후 배포

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { getCurrentUrl } from './utils/get-current-url';
 
 export const middleware = (req: NextRequest) => {
-  // if req.nextUrl.pathname doesn't start with /api, return
   if (!req.nextUrl.pathname.startsWith('/api')) return;
 
-  const url = process.env.QUERY_PATH;
+  const url = getCurrentUrl();
   return NextResponse.rewrite(`${url}/api/bypass/get`);
 };
 
