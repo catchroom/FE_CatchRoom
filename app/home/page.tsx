488f82ce9c8201fb996c3ef42cc9d345c3d4fe02
MinSeobKim <icdmiracle927@gmail.com>
2023-12-20 12:12:00 +0900

    Chore : css, favicon setting

---
+++ app/home/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+	return <div className="w-full">page</div>;
+};
+
+export default page;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: false,
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/home',
+				permanent: true,
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
