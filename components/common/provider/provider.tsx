b4905d31598c99ffbc006a195693ecc592a474a0
MinSeobKim <icdmiracle927@gmail.com>
2023-12-20 15:06:05 +0900

    Chore : recoil 설치

--- components/common/provider/provider.tsx
+++ components/common/provider/provider.tsx
@@ -3,6 +3,7 @@
 import React, { ReactNode, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RecoilRoot } from 'recoil';
 
 const ClientProvider = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(
@@ -18,8 +19,10 @@ const ClientProvider = ({ children }: { children: ReactNode }) => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <RecoilRoot>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </RecoilRoot>
     </QueryClientProvider>
   );
 };
