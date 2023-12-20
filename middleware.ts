e3dd3ee0003aea5cd1d1e4460193ad992d0cf8d0
MinSeobKim <icdmiracle927@gmail.com>
2023-12-20 22:38:05 +0900

    Test : middleware docker image 변경

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 export const middleware = async (req: NextRequest) => {
   if (!req.nextUrl.pathname.startsWith('/api')) return;
 
+  console.log('middleware');
+
   const url =
     process.env.NODE_ENV === 'production'
       ? 'https://main.dhlbrqe2v28e4.amplifyapp.com/'
