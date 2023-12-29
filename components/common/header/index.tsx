d6f8a94111d4a3d0fffe9b22c2b7f78729bfb81c
suyeonnnnnnn <suyeon9923@naver.com>
2023-12-29 18:33:21 +0900

    ✨ feat: header 아이콘을 svg파일로 변경

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,6 +1,10 @@
 'use client';
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import BackIcon from '../../../public/svg/chevron-left.svg';
+import CloseIcon from '../../../public/svg/ph_x.svg';
+import MoreIcon from '../../../public/svg/dots-vertical.svg';
+import Image from 'next/image';
 
 type HeaderProps = {
   title: string;
@@ -27,17 +31,19 @@ const Header = ({
     <header className={headerClass}>
       {showBackButton && (
         <button onClick={() => router.back()} className="text-h2">
-          ←{/* 아이콘으로 변경 예정 */}
+          <Image src={BackIcon} alt="뒤로가기 아이콘" />
         </button>
       )}
       {showCloseButton && (
         <button onClick={() => router.back()} className="text-h2">
-          X{/* 아이콘으로 변경 예정 */}
+          <Image src={CloseIcon} alt="닫기 아이콘" />
         </button>
       )}
       <h1 className="text-h2 font-semibold">{title}</h1>
       {showMoreButton && (
-        <button className="text-h2">⋮{/* 아이콘으로 변경 예정 */}</button>
+        <button className="text-h2">
+          <Image src={MoreIcon} alt="더보기 아이콘" />
+        </button>
       )}
     </header>
   );
