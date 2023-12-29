68def523b0404265a3657847da698398b1a79d71
junkue20 <junkue13@gmail.com>
2023-12-29 17:35:46 +0900

    📦 chore : 더미데이터용 상수 추가

---
+++ constants/serchpage.ts
@@ -0,0 +1,20 @@
+export const SEARCH_DEFAULT = {
+  props: [
+    {
+      icon: 'pin',
+      placeholder: '지역',
+    },
+    {
+      icon: 'calendar',
+      placeholder: '날짜',
+    },
+    {
+      icon: 'person',
+      placeholder: '인원 수',
+    },
+    {
+      icon: 'house',
+      placeholder: '숙소 종류',
+    },
+  ],
+};
