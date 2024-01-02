c23f2966eb304ded49b3c0eab4e5fb4a50b62c91
minseokiim <alstj991226@naver.com>
2024-01-02 19:19:59 +0900

    📦 feat: 로그인 페이지

--- app/login/email/page.tsx
+++ app/login/email/page.tsx
@@ -1,7 +1,76 @@
-import React from 'react';
+'use client';
+import Header from '@/components/common/header';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import Link from 'next/link';
 
-function page() {
-  return <div>로그인페이징</div>;
+type LoginData = {
+  email: string;
+  password: string;
+};
+
+function Page() {
+  const [state, setState] = useState<LoginData>({
+    email: '',
+    password: '',
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginData>();
+
+  const onSubmit = (data: LoginData) => {
+    setState(data);
+    console.log(state);
+  };
+
+  return (
+    <>
+      <Header title="로그인" showBackButton />
+      <div className="flex w-screen h-screen px-4 py-6 mx-auto">
+        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="이메일"
+            {...register('email', {
+              required: '이메일과 비밀번호를 입력하세요.',
+            })}
+            className="border-b-2 w-4/5 mx-auto border-gray-400 focus:border-pink-500 mb-2"
+          />
+
+          {errors.password || errors.email ? (
+            <div className="text-raspberry">
+              {errors.password && <p>{errors.password.message}</p>}
+            </div>
+          ) : null}
+
+          <input
+            placeholder="비밀번호"
+            type="비밀번호"
+            {...register('password', {
+              required: '이메일과 비밀번호를 입력해주세요.',
+            })}
+            className="w-4/5 mx-auto border-b-2 border-gray-400 focus:border-pink-500 mb-2"
+          />
+
+          <div>
+            <button className="mt-10 pink-600" type="submit">
+              로그인
+            </button>
+          </div>
+
+          <div className="flex justify-between ">
+            <span>
+              <Link href="">비밀번호 재설정</Link>
+            </span>
+            <span>
+              <Link href="">이메일로 회원가입</Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default page;
+export default Page;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,40 +1,40 @@
 'use client';
 
 import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
-import { useGoogleLogin } from '@react-oauth/google';
+// import { GoogleLogin } from '@react-oauth/google';
+// import { useGoogleLogin } from '@react-oauth/google';
 import Image from 'next/image';
-import MyCustomButton from './_components/MyCustomButton';
-import { getSocialAuth } from '@/api/socialAuth';
+// import MyCustomButton from './_components/MyCustomButton';
+// import { getSocialAuth } from '@/api/socialAuth';
 import KakaoLoginButton from './_components/KakaoLoginButton';
 import { useRouter } from 'next/navigation';
 
 const Page = () => {
   const router = useRouter();
 
-  const login = useGoogleLogin({
-    onSuccess: (res) => {
-      console.log('서버에 보낼 값', res.code.toString());
+  // const login = useGoogleLogin({
+  //   onSuccess: (res) => {
+  //     console.log('서버에 보낼 값', res.code.toString());
 
-      getSocialAuth(res.code.toString())
-        .then((response) => {
-          console.log('구글 인증 성공', response.data);
-        })
-        .catch((error) => {
-          console.error('구글 인증 실패', error);
-        });
-    },
-    flow: 'auth-code',
-  });
+  //     getSocialAuth(res.code.toString())
+  //       .then((response) => {
+  //         console.log('구글 인증 성공', response.data);
+  //       })
+  //       .catch((error) => {
+  //         console.error('구글 인증 실패', error);
+  //       });
+  //   },
+  //   flow: 'auth-code',
+  // });
 
   // 백엔드에서 Authorization Code를 받아 토큰을 얻는 작업 처리 필요
 
   return (
-    <div className="flex flex-col items-center w-screen h-screen">
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
       {/* 로고 */}
 
       <Image
-        className="justify-center mt-20"
+        className="justify-center"
         src="/Logo.png"
         alt="Logo"
         width={150}
@@ -46,19 +46,19 @@ const Page = () => {
       </div>
 
       {/* 커스텀 적용 */}
-      <div className="justify-center mt-4">
+      {/* <div className="justify-center mt-4">
         <MyCustomButton onClick={() => login()} />
-      </div>
+      </div> */}
 
       {/* 구글 인가코드 */}
-      <div className="justify-center mt-4">
+      {/* <div className="justify-center mt-4">
         <GoogleLogin
           onSuccess={login}
           onError={() => {
             console.log('구글 로그인 실패');
           }}
         />
-      </div>
+      </div> */}
 
       {/* 이메일*/}
       <div className="w-3/4 mt-5">
