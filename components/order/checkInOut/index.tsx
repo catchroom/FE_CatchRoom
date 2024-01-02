dfb7724fbc2de4a12d796b7f66134e15b75bf649
suyeonnnnnnn <suyeon9923@naver.com>
2024-01-02 17:37:27 +0900

    🎨 style: CheckInOut 텍스트 크기 수정

--- components/order/checkInOut/index.tsx
+++ components/order/checkInOut/index.tsx
@@ -15,15 +15,14 @@ const CheckInOut = ({ checkIn, checkOut }: CheckInOutProps) => {
   return (
     <div className="flex justify-between items-center bg-gray-300 mb-4 px-10 py-2 ">
       <div className="text-center">
-        <div className="text-sm font-medium text-gray-600">체크인</div>
-        <div className="text-lg font-bold text-gray-800">{checkIn.date}</div>
-        <div className="text-md font-bold text-gray-800">{checkIn.time}</div>
+        <div className="text-p1 font-medium text-gray-600">체크인</div>
+        <div className="text-p1 font-bold text-gray-800">{checkIn.date}</div>
+        <div className="text-p1 font-bold text-gray-800">{checkIn.time}</div>
       </div>
-      <div className="border-r border-gray-300 mx-4"></div>
       <div className="text-center">
-        <div className="text-sm font-medium text-gray-600">체크아웃</div>
-        <div className="text-lg font-bold text-gray-800">{checkOut.date}</div>
-        <div className="text-md font-bold text-gray-800">{checkOut.time}</div>
+        <div className="text-p1 font-medium text-gray-600">체크아웃</div>
+        <div className="text-p1 font-bold text-gray-800">{checkOut.date}</div>
+        <div className="text-p1 font-bold text-gray-800">{checkOut.time}</div>
       </div>
     </div>
   );
