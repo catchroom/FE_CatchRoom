8ee38b756d3a21f32eb2e4d334dc6c1730778139
junkue20 <junkue13@gmail.com>
2024-01-03 11:31:18 +0900

    🎨  style : svg 파일 및 컴포넌트 추가

---
+++ public/svgComponent/leftArrow.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+const LeftArrowComponent = () => (
+  <svg
+    width={28}
+    height={28}
+    viewBox="0 0 28 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M17.5 21L10.5 14L17.5 7"
+      stroke="black"
+      strokeWidth={1.2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default LeftArrowComponent;
