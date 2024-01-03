1a8a97329802bdae9b0ed2582ed3577612e2c2ea
suyeonnnnnnn <suyeon9923@naver.com>
2024-01-03 19:48:12 +0900

    ✨ feat: 버튼 추가

--- app/order/complete/page.tsx
+++ app/order/complete/page.tsx
@@ -2,6 +2,8 @@ import CompleteMessage from '@/components/complete/completeMessage';
 import ProductDetails from '@/components/complete/productDetails';
 import ReservationInfo from '@/components/complete/reservationInfo';
 import CheckInOut from '@/components/order/checkInOut';
+import ActionButton from '@/components/order/modal/actionButton';
+import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
@@ -29,7 +31,7 @@ const page = () => {
   };
   return (
     <>
-      <div className="flex flex-col container mx-auto w-full px-5 pt-14 bg-bg">
+      <div className="flex flex-col container mx-auto w-full px-5 pt-14 pb-11 bg-bg">
         <CompleteMessage />
         <ProductDetails
           accommodationName={accommodationName}
@@ -41,6 +43,21 @@ const page = () => {
           guest={bookingDetails.guest}
           totalPrice={bookingDetails.totalPrice}
         />
+        <div className="flex flex-row justify-center items-stretch gap-6 mt-28">
+          <Link href="/home">
+            <ActionButton
+              label="홈으로 이동"
+              colorClass="bg-white text-black border border-black"
+            />
+          </Link>
+          <Link href="/details" passHref>
+            {/* 상세보기 url 확인 후 수정 필요 */}
+            <ActionButton
+              label="상세보기"
+              colorClass="bg-gray-1000 text-white"
+            />
+          </Link>
+        </div>
       </div>
     </>
   );
