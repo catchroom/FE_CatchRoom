4d1b480c527bb1fd6deed1072da75dd96bd4b486
MinSeobKim <icdmiracle927@gmail.com>
2024-01-04 18:49:49 +0900

    ✨ feat : menu 버튼 생성

---
+++ app/mypage/dashboard/sales/page.tsx
@@ -0,0 +1,12 @@
+import Menu from '@/components/mypage/menu';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+};
+
+export default page;
