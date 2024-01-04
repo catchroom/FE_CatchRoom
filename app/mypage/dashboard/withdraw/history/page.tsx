3730c71ac1b206b2001a686c90add2b9872a419a
MinSeobKim <icdmiracle927@gmail.com>
2024-01-04 16:40:59 +0900

    ✨ feat : getDate 유틸 함수 제작

---
+++ app/mypage/dashboard/withdraw/history/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="w-full h-full mt-5 px-5">
+      <div>
+        <p>(출금한 날짜)</p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
