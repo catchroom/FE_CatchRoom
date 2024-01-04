24daeaa1084f1d3525c5c0e46791019d71333cdc
jimin <moana16@naver.com>
2024-01-04 17:39:37 +0900

    ⚡️ feat : 홈 화면 전체보기 클릭 시 후기 페이지로 이동

--- components/home/review/reviewContainer/index.tsx
+++ components/home/review/reviewContainer/index.tsx
@@ -2,13 +2,22 @@
 
 import React from 'react';
 import ReviewSwiper from '../reviewSwiper';
+import { useRouter } from 'next/navigation';
 
 const ReviewContainer = () => {
+  const router = useRouter();
+
+  const handleClickViewAllBtn = () => {
+    router.push('/review');
+  };
   return (
     <div className="w-full mt-16 pb-14">
       <div className="flex justify-between my-3">
         <h1 className="text-h4 font-bold">이용후기</h1>
-        <span className="underline decoration-solid cursor-pointer text-t3 text-text-sub">
+        <span
+          onClick={handleClickViewAllBtn}
+          className="underline decoration-solid cursor-pointer text-t3 text-text-sub"
+        >
           전체보기
         </span>
       </div>
