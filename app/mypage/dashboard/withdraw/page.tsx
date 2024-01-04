17b4cc02296066a171365311619dec09b07f1380
MinSeobKim <icdmiracle927@gmail.com>
2024-01-04 16:26:42 +0900

    ✨ feat : 출금 페이지 제작

--- app/mypage/dashboard/withdraw/page.tsx
+++ app/mypage/dashboard/withdraw/page.tsx
@@ -1,12 +1,21 @@
-import BottomSheets from '@/components/common/bottomSheets';
+import WithdrawForm from '@/components/mypage/form/withdrawForm';
 import React from 'react';
+
+const userProfile = {
+  bank: '신한은행',
+  account: '110-123-456789',
+  balance: 1000000,
+};
 
 const page = () => {
   return (
-    <div className="w-full px-5">
-      <BottomSheets title="버튼클릭">
-        <div>hello</div>
-      </BottomSheets>
+    <div className="w-full h-full px-5 mt-5">
+      <div className="flex gap-3">
+        <h1>{userProfile.bank}</h1>
+        <p>{userProfile.account}</p>
+      </div>
+      <WithdrawForm originalBalance={userProfile.balance} />
+      <p>출금가능 금액 : {userProfile.balance}원</p>
     </div>
   );
 };
