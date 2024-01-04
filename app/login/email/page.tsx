b24c8441a79be6661e452815690070dec3acb536
minseokiim <alstj991226@naver.com>
2024-01-05 00:37:02 +0900

    ✨  feat: 로그인 레이아웃

--- app/login/email/page.tsx
+++ app/login/email/page.tsx
@@ -26,46 +26,39 @@ const Page = () => {
   };
 
   return (
-    <div className="w-[350px] mr-10 pr-10">
+    <>
       <Header title="이메일로 로그인" showBackButton />
-      <div className="w-full h-full flex flex-col px-5 mt-5 items-center bg-primary">
-        <div className="flex w-screen h-screen mx-auto">
-          <form
-            className="w-[450px] flex flex-col gap-2"
-            onSubmit={handleSubmit(onSubmit)}
-          >
+      <div className="flex flex-col container mx-auto px-3 py-6 bg-primary">
+        <div>
+          <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
             <input
               placeholder="이메일"
               {...register('email', {
-                required: '이메일과 비밀번호를 입력하세요.',
+                required: '이메일을 입력하세요.',
               })}
-              className="w-[350px] h-[52px] border-2 gap-6 border-gray-400 focus:border-pink-700 mb-2 flex flex-col items-start self-stretch rounded-md"
+              className="w-full h-[3rem] border-2 border-gray-400 focus:border-pink-700 mb-3 flex flex-col items-start pl-3 rounded-md"
             />
 
-            {errors.password || errors.email ? (
-              <div className="text-raspberry">
-                {errors.password && <p>{errors.password.message}</p>}
-              </div>
-            ) : null}
-
+            {errors.email && <p>{errors.email.message}</p>}
             <input
               placeholder="비밀번호"
               type="password"
               {...register('password', {
-                required: '이메일과 비밀번호를 입력해주세요.',
+                required: '비밀번호를 입력해주세요.',
               })}
-              className="w-[350px] h-[52px] border-2 gap-2 border-gray-400 focus:border-pink-700 mb-2 flex flex-col items-start  rounded-md"
+              className="w-full h-[3rem] border-2 border-gray-400 focus:border-pink-700 mb-3 flex flex-col items-start  pl-3 rounded-md"
             />
+            {errors.password && <p>{errors.password.message}</p>}
 
             <button
-              className="w-[350px] h-[52px] bg-focus  font-pretend text-t2 font-medium text-text-on rounded-md mt-7"
+              className="w-full h-[3rem]  bg-focus  font-pretend text-t2 font-medium text-text-on rounded-md mt-7"
               type="submit"
             >
               로그인
             </button>
 
-            <div className="w-[350px] flex justify-between px-5 mt-7 text-p2">
-              <span className="pl-7">
+            <div className="w-full h-[3rem] text-gray-600 flex justify-between px-5 mt-7 text-p2">
+              <span className="pl-11">
                 <Link
                   href="https://www.yanolja.com/"
                   className="hover:underline"
@@ -73,7 +66,8 @@ const Page = () => {
                   비밀번호 재설정
                 </Link>
               </span>
-              <span className="pr-8">
+              |
+              <span className="pr-11">
                 <Link href="/signup" className="hover:underline">
                   이메일로 회원가입
                 </Link>
@@ -82,7 +76,7 @@ const Page = () => {
           </form>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
