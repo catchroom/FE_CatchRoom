c0d3277268499365c622e3f375e8f843971561d0
junkue20 <junkue13@gmail.com>
2024-01-07 01:13:48 +0900

    🎨 style : 체크박스 svg 컴포넌트 추가

---
+++ public/svgComponent/checkbox/default.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+const ChkBoxDefaultIcon = () => (
+  <svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0.5 4C0.5 2.067 2.067 0.5 4 0.5H20C21.933 0.5 23.5 2.067 23.5 4V20C23.5 21.933 21.933 23.5 20 23.5H4C2.067 23.5 0.5 21.933 0.5 20V4Z"
+      fill="white"
+      stroke="#DBDEE3"
+    />
+    <path
+      d="M18 8L9.75 17L6 12.9091"
+      stroke="#BCC0C6"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default ChkBoxDefaultIcon;
