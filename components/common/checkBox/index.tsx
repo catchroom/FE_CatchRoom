e500de98197b251bbe60e31c7d7266dd121e9f6b
junkue20 <junkue13@gmail.com>
2024-01-08 12:54:46 +0900

    📦 chore : JSDoc 수정

--- components/common/checkBox/index.tsx
+++ components/common/checkBox/index.tsx
@@ -9,9 +9,9 @@ import { CheckBoxPropsTypes } from '@/types/common/checkbox/types';
  * @summary - 라벨을 클릭했을때도 체크박스가 선택되도록 만들었습니다.
  * @param id - 체크박스와 라벨의 식별을 위한 id값입니다. `ex) selectAll, agreeTerms 등 유니크한 이름으로 자유롭게 지정이 가능합니다.` (필수)
  * @param labelText -체크박스 우측에 표시되는 라벨입니다. (필수)
+ * @param isBoxChecked - 별도로 체크박스의 체크 State를 커스텀하고 싶은 경우, boolean 값으로 전달할 수 있습니다. (필수)
  * @param isLabelTextBold - boolean 값으로 라벨의 텍스트 굵기 유무를 결정할 수 있습니다. `기본값: font-medium` (선택)
  * @param isLabelTextUnderline - boolean 값으로 라벨 텍스트에 밑줄 효과를 줄 수 있습니다. (선택)
- * @param isBoxChecked - 별도로 체크박스의 체크 State를 커스텀하고 싶은 경우, boolean 값으로 전달할 수 있습니다. `기본값: false` (선택)
  * @param handleSelectState - 전체선택 기능을 위해 핸들러 함수를 전달하는 용도로 사용할 수 있습니다. (선택)
  * @param setChkBoxDisabled - boolean 값으로 체크박스의 disabled를 활성화할 수 있습니다. (선택)
  * @returns
@@ -20,9 +20,9 @@ import { CheckBoxPropsTypes } from '@/types/common/checkbox/types';
 const CheckBoxComponent = ({
   id,
   labelText,
+  isBoxChecked,
   isLabelTextBold = false,
   isLabelTextUnderline = false,
-  isBoxChecked,
   handleSelectState,
   setChkBoxDisabled,
 }: CheckBoxPropsTypes) => {
