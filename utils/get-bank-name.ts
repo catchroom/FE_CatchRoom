b334751880c0d3880e527d249a443a4cefb46515
MinSeobKim <icdmiracle927@gmail.com>
2024-01-08 10:20:59 +0900

    feat : 오류사항 해결

--- utils/get-bank-name.ts
+++ utils/get-bank-name.ts
@@ -1,5 +1,11 @@
 import { BANK_LIST } from '@/constants/mypage';
 
+/**
+ * @function getBankName - 은행 코드를 받아서 은행 이름을 반환합니다.
+ * @param bankCode - 잘 사용하세요
+ * @returns
+ */
+
 export const getBankName = (bankCode: string) => {
   const bank = BANK_LIST.find((bank) => bank.value === bankCode);
   return bank?.name;
