aadf86e8823234d1e2bc2e7029399a8c54dcd056
ewinkite <139189610+ewinkite@users.noreply.github.com>
2024-01-09 20:54:58 +0900

    ✨ feat : 비로그인시 노출되는 페이지 추가

---
+++ components/chat/chatList/login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <p className="pt-80 text-t2 font-semibold text-text-sub text-center  ">
+      <span className="underline">로그인</span> 후 이용가능합니다.
+    </p>
+  );
+};
+
+export default Login;
