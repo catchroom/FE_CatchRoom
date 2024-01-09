d12f42de9c4a1226db7d83524efe96ce10bd8f69
ewinkite <139189610+ewinkite@users.noreply.github.com>
2024-01-09 20:54:15 +0900

    ✨ feat : header, bottomNav 반영

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ChatList from '../../components/chat/chatList/index';
 import Header from '@/components/common/header';
+import BottomNav from '@/components/common/bottomNav';
 
 const page = () => {
   return (
     <div>
-      <Header title="채팅" />
+      <Header title="채팅" showBackButton />
       <ChatList />
+      <BottomNav />
     </div>
   );
 };
