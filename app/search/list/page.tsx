3ff463abe5d7ea0e77260ad27e7046e117491246
suyeonnnnnnn <suyeon9923@naver.com>
2024-01-11 06:24:25 +0900

    ✨ feat: ProductListControls 생성

---
+++ app/search/list/page.tsx
@@ -0,0 +1,8 @@
+import ProductListControls from '@/components/search/list/productListControls';
+import React from 'react';
+
+const page = () => {
+  return <ProductListControls />;
+};
+
+export default page;
