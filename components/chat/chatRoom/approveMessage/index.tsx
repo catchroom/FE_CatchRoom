aeded3d79d7e7f41d55fb238fc19ff62c0655407
ewinkite <139189610+ewinkite@users.noreply.github.com>
2024-01-10 17:58:56 +0900

    🎨 design : 디자인 변경

--- components/chat/chatRoom/approveMessage/index.tsx
+++ components/chat/chatRoom/approveMessage/index.tsx
@@ -42,7 +42,7 @@ const AppoveMessage = ({ approvePrice, date, isSeller }: MessageProps) => {
           />
           <div className="float-right w-full p-4">
             <p className="text-t2 font-semibold">제안이 승인됐어요</p>
-            <p className="text-p2 text-gray-500">
+            <p className="text-p2 text-gray-500 mt-2 mb-4">
               아래 결제하기 버튼을 통해 결제를 진행해주세요. (변경된 금액은 해당
               채팅창 내에서만 유효합니다.)
             </p>
