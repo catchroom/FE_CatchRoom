c7a8fbe114b96df863b0a1c65f0c06e838a4bd27
suyeonnnnnnn <suyeon9923@naver.com>
2024-01-11 10:20:02 +0900

    🎨 style: ToggleViewButtonWrapper를 fixed로 수정

--- components/search/map/toggleViewButtonWrapper/index.tsx
+++ components/search/map/toggleViewButtonWrapper/index.tsx
@@ -7,7 +7,7 @@ const ToggleViewButtonWrapper = ({
   onViewChange,
 }: ToggleViewButtonWrapperProps) => {
   return (
-    <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 z-20">
+    <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2 z-20">
       <ToggleViewButton currentView={currentView} onViewChange={onViewChange} />
     </div>
   );
