e1b7bf6b12c39fa1996335874784229614c70ff0
minseokiim <alstj991226@naver.com>
2024-01-11 11:42:15 +0900

    ✨ feat: 로그인 페이지 수정 및 소셜로그인 삭제

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,33 +1,15 @@
-import React from 'react';
-import Image from 'next/image';
-import KakaologinImage from '../../public/KakaoLoginButton.png';
-import loginImage from '../../public/LoginButton.png';
-import Link from 'next/link';
 import Header from '@/components/common/header';
-import Logo from '../../public/Logo.png';
-import { kakaoUrl } from '@/api/socialAuth';
+import React from 'react';
+import LoginForm from '@/components/login';
 
 const Page = () => {
   return (
-    <div className="w-full px-1">
-      <Header title="" showBackButton />
-
-      <div className="flex flex-col  items-center justify-center px-3 pt-20 bg-bg">
-        <Image className="justify-center" alt="Logo" src={Logo} />
-
-        <div className="mt-10">
-          <Link href={kakaoUrl}>
-            <Image src={KakaologinImage} alt="카카오로 시작하기" />
-          </Link>
-        </div>
-
-        <div className="mt-3">
-          <Link href="/login/email">
-            <Image src={loginImage} alt="이메일로 시작하기" />
-          </Link>
-        </div>
+    <>
+      <Header title="이메일로 로그인" showBackButton />
+      <div className="flex flex-col container mx-auto px-3 py-6 bg-primary">
+        <LoginForm />
       </div>
-    </div>
+    </>
   );
 };
 
