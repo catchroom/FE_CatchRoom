f8d62c96b471a1c7df6ec0fdec8d775e69651304
minseokiim <alstj991226@naver.com>
2024-01-11 12:40:24 +0900

    🎨 style: 로그인폼 위치 조정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import LoginForm from '@/components/login';
 
 const Page = () => {
   return (
-    <div className="flex flex-col items-center justify-center px-3 pt-20 bg-bg">
+    <div className="flex flex-col items-center justify-center px-3 pt-20 mt-20 bg-bg">
       <Image className="justify-center" alt="Logo" src={Logo} />
       <div className="flex flex-col container mx-auto py-6 bg-primary pt-10">
         <LoginForm />
