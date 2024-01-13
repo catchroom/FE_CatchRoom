f47df4f106cddda736a1300582b3dd77fac2607d
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-14 00:51:09 +0900

    ✨ feat: TermsAndConditions에 CheckBoxComponent 반영

--- components/order/termsAndConditions/index.tsx
+++ components/order/termsAndConditions/index.tsx
@@ -1,12 +1,83 @@
-import { TermsAndConditionsProps } from '@/types/order/termsAndConditions/type';
-import React from 'react';
+'use client';
+import CheckBoxComponent from '@/components/common/checkBox';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 
-const TermsAndConditions = ({ label }: TermsAndConditionsProps) => {
+type TermOption = {
+  id: string;
+  text: string;
+  detailLink: string;
+};
+
+const termsOptions: TermOption[] = [
+  {
+    id: 'serviceTerms',
+    text: '서비스 이용 및 취소/환불규정 동의 (필수)',
+    detailLink: '/mypage/dashboard/terms',
+  },
+  {
+    id: 'personalInfo',
+    text: '개인정보 수집 및 이용 동의 (필수)',
+    detailLink: '/mypage/dashboard/terms',
+  },
+  {
+    id: 'thirdParty',
+    text: '개인정보 제3자 제공 동의 (필수)',
+    detailLink: '/mypage/dashboard/terms',
+  },
+]; //페이지 생성 후 링크 수정 예정
+
+const TermsAndConditions = () => {
+  const [selectedTerms, setSelectedTerms] = useState<string[]>([]);
+  const [isAllSelected, setIsAllSelected] = useState(false);
+
+  useEffect(() => {
+    if (isAllSelected) {
+      setSelectedTerms(termsOptions.map((option) => option.id));
+    } else {
+      setSelectedTerms([]);
+    }
+  }, [isAllSelected]);
+
+  const handleSelectTerm = (id: string) => {
+    setSelectedTerms((prevSelected) =>
+      prevSelected.includes(id)
+        ? prevSelected.filter((term) => term !== id)
+        : [...prevSelected, id],
+    );
+  };
+
+  useEffect(() => {
+    setIsAllSelected(
+      termsOptions.every((option) => selectedTerms.includes(option.id)),
+    );
+  }, [selectedTerms]);
+
   return (
-    <div className="flex items-center mt-8 text-p2 font-bold text-gray-1000">
-      <label>
-        <input type="checkbox" /> {label}
-      </label>
+    <div className="flex-col space-y-4 py-5 mt-6">
+      <CheckBoxComponent
+        id="selectAll"
+        labelText="전체 동의"
+        isBoxChecked={isAllSelected}
+        isLabelTextBold={true}
+        handleSelectState={() => setIsAllSelected(!isAllSelected)}
+      />
+      {termsOptions.map(({ id, text, detailLink }) => (
+        <div key={id} className="flex justify-between items-center relative">
+          <CheckBoxComponent
+            id={id}
+            labelText={text}
+            isBoxChecked={selectedTerms.includes(id)}
+            handleSelectState={() => handleSelectTerm(id)}
+          />
+          <Link
+            href={detailLink}
+            className="text-text-primary  absolute right-0 text-t2"
+          >
+            보기
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
