39ec9a94f774406deae6d967d8fdd09fad6eebf4
junkue20 <junkue13@gmail.com>
2024-01-14 14:39:49 +0900

    ♻️ refactor : weekCalendar 컴포넌트 적용 및 atom 추가

--- atoms/searchroom/checkbox.ts
+++ atoms/searchroom/checkbox.ts
@@ -1,11 +1,23 @@
 import { atom } from 'recoil';
 
-export const regionCheckedState = atom({
-  key: 'isRegionChecked',
-  default: true,
+// 아래 atom은 추후 코드 수정 후 삭제예정
+
+// export const regionCheckedState = atom<boolean>({
+//   key: 'isRegionChecked',
+//   default: true,
+// });
+
+// export const roomCheckedState = atom<boolean>({
+//   key: 'isRoomChecked',
+//   default: true,
+// });
+
+export const adultCountState = atom<number>({
+  key: 'adultCountState',
+  default: 2,
 });
 
-export const roomCheckedState = atom({
-  key: 'isRoomChecked',
-  default: true,
+export const childCountState = atom<number>({
+  key: 'childCountState',
+  default: 0,
 });
