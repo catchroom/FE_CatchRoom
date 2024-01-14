4db9a7ee64f435933ce1b29b06e34aa2485b2ba4
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-15 05:10:45 +0900

    🎨 style: Disclaimer 글자색 변경

--- components/order/modal/disclaimer/index.tsx
+++ components/order/modal/disclaimer/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Disclaimer = () => (
   <div className="flex flex-col mt-9">
-    <p className="text-t2">
+    <p className="text-text-sub text-t2">
       ∙ 19세 미만 청소년은 보호자 동반 시 투숙이 가능합니다.
     </p>
-    <p className="text-t2">
+    <p className="text-text-sub text-t2">
       ∙ 취소/환불 규정에 따라 환불이 불가능한 상품입니다.
     </p>
   </div>
