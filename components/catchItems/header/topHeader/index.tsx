2fe56ac0a6527b3fa335e515e87b598a653aa956
junkue20 <junkue13@gmail.com>
2024-01-14 14:45:18 +0900

    ♻️ refactor : 스타일 수정 및 불필요 코드 삭제

--- components/catchItems/header/topHeader/index.tsx
+++ components/catchItems/header/topHeader/index.tsx
@@ -1,5 +1,4 @@
 'use client';
-import DownArrowIcon from '@/public/svgComponent/downArrow';
 import LeftArrowComponent from '@/public/svgComponent/leftArrow';
 import { useRouter } from 'next/navigation';
 import React from 'react';
@@ -11,15 +10,12 @@ const TopHeader = () => {
     router.back();
   };
   return (
-    <div className="relative h-[3.25rem] flex py-3 px-4 items-center justify-center">
+    <div className="relative h-[3.25rem] flex items-center justify-center">
       <button className="absolute left-0 ml-3 p-3" onClick={backPageHandler}>
         <LeftArrowComponent />
       </button>
       <button className="flex items-center justify-center text-t1 font-bold">
         2023년 12월
-        <p className="flex items-center justify-center w-[1rem] h-[1rem] ml-1">
-          <DownArrowIcon width={12} height={7} />
-        </p>
       </button>
     </div>
   );
