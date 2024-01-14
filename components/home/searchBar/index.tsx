a18ab92106ca1e7fe0847670191d5015ef96f5ac
junkue20 <junkue13@gmail.com>
2024-01-14 14:46:43 +0900

    ♻️ refactor : 메인홈 스타일 및 이미지 alt 수정

--- components/home/searchBar/index.tsx
+++ components/home/searchBar/index.tsx
@@ -17,7 +17,7 @@ const SearchBar = () => {
       onClick={pageHandler}
     >
       <p>지역, 여행 일정, 숙소 유형으로 검색해보세요</p>
-      <Image src="/search-md.svg" alt="seachIcon" width={24} height={24} />
+      <Image src="/search-md.svg" alt="searchIcon" width={24} height={24} />
     </button>
   );
 };
