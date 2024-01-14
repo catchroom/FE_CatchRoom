c79d41dc8c7cc9e8ab56c53f8b712f44453c753a
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-15 04:02:56 +0900

    ✨ feat: BookingDetails 생성

---
+++ components/order/modal/bookingDetails/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { CheckInComponentProps } from '@/types/common/checkInComponent/types';
+import { CHECKIN_TIME, CHECKOUT_TIME } from '@/constants/CheckInOut';
+import { ProductDetailsProps } from '@/types/order/productDetails/types';
+
+type BookingDetailsProps = ProductDetailsProps & CheckInComponentProps;
+
+const BookingDetails = ({
+  accommodationName,
+  roomName,
+  CheckInDate,
+  CheckOutDate,
+}: BookingDetailsProps) => (
+  <div className="mb-4">
+    <p className="text-t2">{accommodationName}</p>
+    <p className="text-t2">{roomName}</p>
+    <div className="flex flex-col">
+      <div className="flex justify-between mr-14">
+        <p className="text-t2">체크인</p>
+        <p className="font-bold">
+          {CheckInDate} {CHECKIN_TIME}
+        </p>
+      </div>
+      <div className="flex justify-between mr-14">
+        <p className="text-t2">체크아웃</p>
+        <p className="font-bold">
+          {CheckOutDate} {CHECKOUT_TIME}
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default BookingDetails;
--- components/order/modal/index.tsx
+++ components/order/modal/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ActionButton from './actionButton';
 import { ModalProps } from '@/types/order/modal/type';
+import BookingDetails from './bookingDetails';
+import Disclaimer from './disclaimer';
 
 const Modal = ({ isOpen, onClose, onConfirm }: ModalProps) => {
   if (!isOpen) return null;
@@ -8,8 +10,17 @@ const Modal = ({ isOpen, onClose, onConfirm }: ModalProps) => {
   return (
     <div className="fixed inset-0  bg-gray-1000 bg-opacity-50 flex justify-center items-center">
       <div className="relative bg-white w-full max-w-md m-auto">
-        <div className="p-6">
-          <p className="text-center mb-14">모달 내용</p>
+        <div className="p-5">
+          <BookingDetails
+            accommodationName="제주신라호텔"
+            roomName="스탠다드 더블 / 1박"
+            CheckInDate="12월 25일 (월)"
+            CheckOutDate="12월 26일 (화)"
+            normalCapacity={2}
+            maxCapacity={4}
+            imageUrl="room_image_url.jpg"
+          />
+          <Disclaimer />
           <div className="flex justify-center space-x-4 mt-4">
             <ActionButton
               action={onClose}
