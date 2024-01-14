52b58d7147dee52024fc999c0dd74e9d6da1095f
junkue20 <junkue13@gmail.com>
2024-01-14 23:25:32 +0900

    📦 chore : JSDoc 수정

--- hooks/useBtnLoading.ts
+++ hooks/useBtnLoading.ts
@@ -4,8 +4,8 @@ import React, { MouseEventHandler, useEffect } from 'react';
 /**
  * @function useBtnLoading - 버튼
  * @param pushURL - 클릭 시 다음페이지로 넘어갈 URL을 입력해주세요. `ex: /catch-items`
- * @returns { isActive, btnHandler }
- * @example const debounceText = useDebounceText(text, 500);
+ * @returns { isActive, btnHandler } - isActive는 버튼의 상태값, btnHandler는 버튼 핸들러 함수입니다.
+ * @example const { isActive, btnHandler } = useBtnLoading('/catch-items');
  */
 
 export const useBtnLoading = (pushURL: string) => {
