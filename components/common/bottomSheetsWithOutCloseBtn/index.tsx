9a579f2fc5bc4ac73aac0945aec17dbc4565b160
jimin <moana16@naver.com>
2024-01-15 18:25:48 +0900

    🐛 fix : fix JSDoc

--- components/common/bottomSheetsWithOutCloseBtn/index.tsx
+++ components/common/bottomSheetsWithOutCloseBtn/index.tsx
@@ -15,14 +15,9 @@ import DropdownButton from '../sheetsButtons/dropdownButton';
  * @function BottomSheets - bottom sheets component입니다. 모달 대체용으로 사용합니다.
  * @param children - 모달 내부에 들어갈 컴포넌트입니다. (필수)
  * @param title - 모달의 제목입니다. (필수)
- * @param innerTitle - 모달의 제목을 설정하는 props입니다. `기본값: '모달제목'` (선택)
- * @param innerButtonTitle - 모달의 버튼 제목을 설정하는 props입니다. `기본값: '버튼제목'` (선택)
  * @param buttonSelect - 모달을 열기 위한 버튼의 종류를 선택합니다. (선택)
- * @param placeholder - `buttonSelect가 search일 경우` 버튼으로 전달 될 placeholder입니다. (선택)
- * @param icon - `buttonSelect가 search일 경우` 버튼에 표시 될 아이콘입니다. (선택)
  * @summary - buttonSelect의 종류는 'input'과 'simple'이 있습니다. 'input'은 InputButton 컴포넌트를 사용하고, 'simple'은 SimpleButton 컴포넌트를 사용합니다. (default: 'simple')
  * @summary - 버튼을 추가하고 싶다면 components/common/sheetsButtons 폴더에 컴포넌트를 추가하고, buttonSelect에 해당 컴포넌트를 넣어주세요.
- * @param closeButton - 모달 내부에 선택완료 버튼을 추가하고 싶다면 true로 설정해주세요. (선택)
  * @param outerControl - 모달을 외부에서 컨트롤 하고 싶다면 true로 설정해주세요. (선택) outerBottomSheetsControl atom을 사용합니다.
  * @returns
  */
@@ -31,7 +26,7 @@ const BottomSheetsWithoutCloseBtn = ({
   children,
   title,
   buttonSelect = 'dropdown',
-  outerControl = false,
+  outerControl = true,
 }: {
   children: ReactNode;
   title: string;
