c47856d0856efa711b42ebd84bdce9115bf2939b
MinSeobKim <icdmiracle927@gmail.com>
2024-01-15 12:21:46 +0900

    🐛 feat : 게시만료 z 속성 추가

--- components/mypage/items/sellingItems/index.tsx
+++ components/mypage/items/sellingItems/index.tsx
@@ -35,7 +35,7 @@ const MItem = ({ item }: { item: MypageSellingType }) => {
           <div className="relative w-[120px] h-[120px]">
             {/* 호텔 이미지 */}
             {isNotIng && (
-              <div className="absolute flex items-center justify-center inset-0 backdrop-saturate-50 backdrop-brightness-75">
+              <div className="absolute flex z-10 items-center justify-center inset-0 backdrop-saturate-50 backdrop-brightness-75">
                 <p className="text-text-on font-semibold text-t2">판매완료</p>
               </div>
             )}
