7af01a514531af0bf55fa1d23b039f0403969a97
MinSeobKim <icdmiracle927@gmail.com>
2024-01-16 10:41:26 +0900

    ✨ feat : 위시리스트 컴포넌트 추가 및 레이아웃만 작성

---
+++ app/mypage/dashboard/wishlist/page.tsx
@@ -0,0 +1,25 @@
+import CatchSpecialComponent from '@/components/common/catchComponent';
+import WishListDivider from '@/components/mypage/divider/wishListDivider';
+import { ITEMS_INFO } from '@/constants/catchItems';
+import React from 'react';
+
+const page = () => {
+  return (
+    <WishListDivider>
+      {ITEMS_INFO.roomItems.map((item) => {
+        return (
+          <CatchSpecialComponent
+            key={item.roomName}
+            roomName={item.roomName}
+            roomType={item.roomType}
+            resDate={item.resDate}
+            oldPrice={item.oldPrice}
+            discount={item.discount}
+          />
+        );
+      })}
+    </WishListDivider>
+  );
+};
+
+export default page;
