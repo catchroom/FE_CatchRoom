6e10e536793ddfbc28deec59284133751ac08236
MinSeobKim <icdmiracle927@gmail.com>
2024-01-18 22:26:22 +0900

    🧪 test : reviewForm test 코드 작성

--- components/mypage/form/reviewForm/index.test.tsx
+++ components/mypage/form/reviewForm/index.test.tsx
@@ -3,20 +3,48 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ReviewForm from '.';
 
-describe('ProfileForm test', () => {
+describe('ReviewForm test', () => {
   const user = userEvent.setup();
   const renderComponent = () => render(<ReviewForm />);
 
-  it('cancel-button을 누르면 input value가 빈 값이 되어야 함', async () => {
+  it('input에 문자가 100자 이상 들어가지 않는 지 테스트', async () => {
     renderComponent();
 
-    const input = screen.getByRole('textbox');
-    const cancelButton = screen.getByTestId('cancel-button');
+    const textarea = screen.getByRole('textbox');
+    await user.type(textarea, '가'.repeat(200));
 
-    await user.type(input, '홍길동전퐁당퐁당퐁당');
-    await user.click(cancelButton);
     await waitFor(() => {
-      expect(input).toHaveValue('');
+      // 100자가 넘어가면 100자까지만 입력되는 지
+      expect(textarea).toHaveValue('가'.repeat(100));
+
+      // 100자가 넘어가면 경고 문구가 뜨는 지
+      const warningMessage = screen.getByText(
+        '리뷰는 최대 100자까지 입력 가능합니다.',
+      );
+      expect(warningMessage).toBeInTheDocument();
+    });
+  });
+
+  it('placeHorder에 문자가 잘 들어가 있는 지', () => {
+    renderComponent();
+
+    const textarea = screen.getByPlaceholderText(
+      '거래 경험을 자유롭게 작성해주세요. (추후 플랫폼 개선에 큰 도움이 됩니다.)',
+    );
+    expect(textarea).toBeInTheDocument();
+  });
+
+  it('아무것도 입력 안했을 때 경고 문구가 발생하는 지', async () => {
+    renderComponent();
+
+    const submitButton = screen.getByRole('button', { name: '등록' });
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const warningMessage = screen.getByText(
+        '리뷰는 최소 1자 이상 입력해주세요.',
+      );
+      expect(warningMessage).toBeInTheDocument();
     });
   });
 });
