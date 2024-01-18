aaa59d8ebfe0defb9f7cd004f408ba40c8b3aab6
jimin <moana16@naver.com>
2024-01-18 18:17:38 +0900

    🔥 chore : 라이브러리 삭제

--- components/sale/saleEndContainer/index.tsx
+++ components/sale/saleEndContainer/index.tsx
@@ -5,20 +5,7 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { hourState, minuteState, timeState } from '@/atoms/sale/timeAtom';
 import BottomSheetsWithoutCloseBtn from '@/components/common/bottomSheetsWithOutCloseBtn';
 import { outerDatePickerBottomSheetsControl } from '@/atoms/commons/outerBottomSheetsControl';
-// import { MobileTimePicker } from '@mui/x-date-pickers';
-//import { TextField } from '@mui/material';
-
-// import { LocalizationProvider } from '@mui/x-date-pickers';
-// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
-// const CustomToolbar = ({ time }: { time: string }) => (
-//   <>
-//     <div>판매 종료 시간</div>
-//     <div>{time}</div>
-//   </>
-// );
-
 const SaleEndContainer = () => {
-  //const [time, setTime] = useState<string | null>('');
   const time = useRecoilValue(timeState);
   const hour = useRecoilValue(hourState);
   const minute = useRecoilValue(minuteState);
@@ -42,28 +29,6 @@ const SaleEndContainer = () => {
       <p className="text-p1 opacity-50 mt-1 mb-3">
         판매 종료일 이후 판매글은 미노출 됩니다
       </p>
-      {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <MobileTimePicker
-          value={time}
-          onChange={(newValue) => {
-            setTime(newValue);
-          }}
-          minutesStep={1}
-          sx={{
-            borderColor: '#000000',
-          }}
-          className="border-border-sub focus:border-border-sub"
-          // slots={{
-          //   toolbar: CustomToolbar,
-          // }}
-          slotProps={{
-            toolbar: {
-              toolbarFormat: '판매 종료 시간',
-            },
-          }}
-        />
-      </LocalizationProvider> */}
-
       <BottomSheetsWithoutCloseBtn
         buttonSelect="timePicker"
         title={title}
