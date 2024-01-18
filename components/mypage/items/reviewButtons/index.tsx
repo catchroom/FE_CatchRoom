519522ae447a140b92c5106062f416f1ff2e5e76
MinSeobKim <icdmiracle927@gmail.com>
2024-01-18 11:36:30 +0900

    ✨ feat : 리뷰 삭제 시 동작하는 bottomSheets outerControl 추가

--- components/mypage/items/reviewButtons/index.tsx
+++ components/mypage/items/reviewButtons/index.tsx
@@ -4,6 +4,8 @@ import BottomSheets from '@/components/common/bottomSheets';
 import BorderButton from '@/components/common/sheetsButtons/borderButton';
 import { ReviewType, decodeReviewState } from '@/utils/get-dot-date';
 import ReviewContent from '../reviewContent';
+import { useSetRecoilState } from 'recoil';
+import { outerBottomSheetsControl } from '@/atoms/commons/outerBottomSheetsControl';
 
 const ReviewButtons = ({
   id,
@@ -13,7 +15,7 @@ const ReviewButtons = ({
   name?: string;
   isReview: ReviewType;
 }) => {
-  // review FETCH 필요
+  const setBottomSheets = useSetRecoilState(outerBottomSheetsControl);
   const router = useRouter();
   const handleClick = () => {
     router.push(`/mypage/dashboard/review?id=${id}`);
@@ -21,15 +23,21 @@ const ReviewButtons = ({
 
   const reviewTitle = decodeReviewState(isReview);
 
+  const closeBottomSheets = () => {
+    setBottomSheets(false);
+  };
+
   const ReviewCase: Record<ReviewType, JSX.Element> = {
     onReview: (
       <BottomSheets
+        outerControl={true}
+        outerControlAtom="default"
         title={reviewTitle}
         innerTitle="작성한 리뷰"
         buttonSelect="border"
         theme={true}
       >
-        <ReviewContent id={id} />
+        <ReviewContent id={id} fn={closeBottomSheets} />
       </BottomSheets>
     ),
     noReview: <BorderButton fn={handleClick} name={reviewTitle} />,
