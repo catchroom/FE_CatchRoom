b9826ea5ce14de7611e0c4b4919f57c23b5b111c
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-21 04:26:41 +0900

    ✨ feat: filterBar 버튼을 BottomSheet 버튼으로 변경

--- app/search-result/(noSortBtn)/layout.tsx
+++ app/search-result/(noSortBtn)/layout.tsx
@@ -2,29 +2,29 @@ import Header from '@/components/common/header';
 import FilterBar from '@/components/search-result/filterBar';
 import React, { ReactNode } from 'react';
 
-const locations = ['서울', '제주'];
-const checkInDate = '12.1';
-const checkOutDate = '12.2';
-const accommodationType = '호텔';
-const adultsCount = 2;
+// const locations = ['서울', '제주'];
+// const checkInDate = '12.1';
+// const checkOutDate = '12.2';
+// const accommodationType = '호텔';
+// const adultsCount = 2;
 
-const dayOfWeekIn = '목';
-const dayOfWeekOut = '금';
+// const dayOfWeekIn = '목';
+// const dayOfWeekOut = '금';
 
-const locationLabel =
-  locations.length > 1
-    ? `${locations[0]} 외 ${locations.length - 1}건`
-    : locations[0];
-const datesLabel = `${checkInDate} ${dayOfWeekIn} ~ ${checkOutDate} ${dayOfWeekOut}`;
-const accommodationLabel = accommodationType;
-const guestsLabel = `성인 ${adultsCount}명`;
+// const locationLabel =
+//   locations.length > 1
+//     ? `${locations[0]} 외 ${locations.length - 1}건`
+//     : locations[0];
+// const datesLabel = `${checkInDate} ${dayOfWeekIn} ~ ${checkOutDate} ${dayOfWeekOut}`;
+// const accommodationLabel = accommodationType;
+// const guestsLabel = `성인 ${adultsCount}명`;
 
-const filters = [
-  { id: 'location', label: locationLabel },
-  { id: 'date', label: datesLabel },
-  { id: 'accommodation', label: accommodationLabel },
-  { id: 'guests', label: guestsLabel },
-];
+// const filters = [
+//   { id: 'location', label: locationLabel },
+//   { id: 'date', label: datesLabel },
+//   { id: 'accommodation', label: accommodationLabel },
+//   { id: 'guests', label: guestsLabel },
+// ];
 
 const Searchlayout = ({
   children,
@@ -35,7 +35,7 @@ const Searchlayout = ({
   return (
     <>
       <Header title="검색 결과" showBackButton showHomeButton />
-      <FilterBar filters={filters} />
+      <FilterBar />
       <main className="w-full absolute h-full mt-20 ">{children}</main>
     </>
   );
--- components/search-result/filterBar/index.tsx
+++ components/search-result/filterBar/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import FilterButton from '../filterButton';
-import { FilterBarProps } from '@/types/search-result/FilterBar/types';
+'use client';
+import React, { useEffect } from 'react';
+import RegionBottomSheet from '@/components/common/searchBtmSheets/region';
+import CalendarBottomSheet from '@/components/common/searchBtmSheets/calendar';
+import RoomBottomSheet from '@/components/common/searchBtmSheets/room';
+import CustomerBottomSheet from '@/components/common/searchBtmSheets/customer';
+import { useResetSearchAtoms } from '@/hooks/useResetSearchAtoms';
 
-const FilterBar = ({ filters }: FilterBarProps) => {
+const FilterBar = () => {
+  const resetAllSearchAtoms = useResetSearchAtoms();
+
+  useEffect(() => {
+    resetAllSearchAtoms(); // eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="fixed z-20 flex gap-1 overflow-x-auto whitespace-nowrap px-5 pt-2 pb-4 mt-14 bg-surface-sub border-b border-divider-sub">
-      {filters.map((filter) => (
-        <FilterButton key={filter.id} label={filter.label} />
-      ))}
+    <div className="w-full max-w-[480px] fixed z-10 flex  gap-1 overflow-x-auto whitespace-nowrap px-5 pt-2 pb-4 mt-14 bg-surface-sub border-b border-divider-sub">
+      <RegionBottomSheet buttonStyle="dropdown" />
+      <CalendarBottomSheet buttonStyle="dropdown" />
+      <RoomBottomSheet buttonStyle="dropdown" />
+      <CustomerBottomSheet buttonStyle="dropdown" />
     </div>
   );
 };
