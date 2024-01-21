4cecbf655175b9d3e2c089ae5022ca0795448263
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-21 21:48:18 +0900

    ✨ feat: filterButton 삭제

--- components/search-result/filterBar/index.tsx
+++ components/search-result/filterBar/index.tsx
@@ -1,18 +1,11 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import RegionBottomSheet from '@/components/common/searchBtmSheets/region';
 import CalendarBottomSheet from '@/components/common/searchBtmSheets/calendar';
 import RoomBottomSheet from '@/components/common/searchBtmSheets/room';
 import CustomerBottomSheet from '@/components/common/searchBtmSheets/customer';
-import { useResetSearchAtoms } from '@/hooks/useResetSearchAtoms';
 
 const FilterBar = () => {
-  const resetAllSearchAtoms = useResetSearchAtoms();
-
-  useEffect(() => {
-    resetAllSearchAtoms(); // eslint-disable-next-line
-  }, []);
-
   return (
     <div className="w-full max-w-[480px] fixed z-10 flex  gap-1 overflow-x-auto whitespace-nowrap px-5 pt-2 pb-4 mt-14 bg-surface-sub border-b border-divider-sub">
       <RegionBottomSheet buttonStyle="dropdown" />
