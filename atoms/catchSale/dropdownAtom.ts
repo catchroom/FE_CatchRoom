c6f4e08f604122efda001e64fbbbec0dc5442ecb
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-25 02:17:32 +0900

    ✨ feat: dropdownState의 default를 할인률 높은 순으로 변경

--- atoms/catchSale/dropdownAtom.ts
+++ atoms/catchSale/dropdownAtom.ts
@@ -2,7 +2,7 @@ import { atom } from 'recoil';
 
 export const dropdownState = atom<string>({
   key: 'dropdownState',
-  default: '낮은 가격순',
+  default: '할인률 높은 순',
 });
 
 export const searchDropdownState = atom<string>({
