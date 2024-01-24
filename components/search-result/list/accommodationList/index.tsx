ba1ccf83617e0cb5655e8b1d185e9a41ad739e85
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-24 15:30:56 +0900

    ✨ feat: formatToKoreanDate 생성, 적용

--- components/search-result/list/accommodationList/index.tsx
+++ components/search-result/list/accommodationList/index.tsx
@@ -2,6 +2,7 @@ import CatchSpecialComponent from '@/components/common/catchComponent';
 import { useProductInfoPage } from '@/hooks/useProductInfoPage';
 import { Accommodation } from '@/types/search-result/list/types';
 import React from 'react';
+import { formatToKoreanDate } from '@/utils/formatDate';
 
 const AccommodationList = ({
   accommodations = [],
@@ -13,20 +14,26 @@ const AccommodationList = ({
   return (
     <div className=" overflow-y-hidden">
       <div className="w-full flex flex-col gap-12 px-6">
-        {accommodations.map((accommodation) => (
-          <CatchSpecialComponent
-            key={accommodation.productId}
-            image={accommodation.image}
-            accommodationName={accommodation.accommodationName}
-            roomName={accommodation.roomName}
-            resDate={accommodation.checkIn + ' - ' + accommodation.checkOut}
-            catchType={accommodation.catchType}
-            originalPrice={accommodation.originalPrice}
-            discountRate={accommodation.discountRate}
-            sellPrice={accommodation.sellPrice}
-            pageHandler={() => pageHandler()}
-          />
-        ))}
+        {accommodations.map((accommodation) => {
+          const formattedCheckIn = formatToKoreanDate(accommodation.checkIn);
+          const formattedCheckOut = formatToKoreanDate(accommodation.checkOut);
+          const resDate = `${formattedCheckIn} - ${formattedCheckOut}`;
+
+          return (
+            <CatchSpecialComponent
+              key={accommodation.productId}
+              image={accommodation.image}
+              accommodationName={accommodation.accommodationName}
+              roomName={accommodation.roomName}
+              resDate={resDate}
+              catchType={accommodation.catchType}
+              originalPrice={accommodation.originalPrice}
+              discountRate={accommodation.discountRate}
+              sellPrice={accommodation.sellPrice}
+              pageHandler={() => pageHandler()}
+            />
+          );
+        })}
       </div>
     </div>
   );
