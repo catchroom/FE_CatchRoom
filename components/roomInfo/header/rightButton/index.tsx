107e9a778cff1f42e5df20fcfa37e1ca0ec34df7
junkue20 <junkue13@gmail.com>
2024-01-24 11:07:58 +0900

    📦 chore : 경로 변경 및 다음 작업내용 주석

--- components/roomInfo/header/rightButton/index.tsx
+++ components/roomInfo/header/rightButton/index.tsx
@@ -10,8 +10,6 @@ const RightButton = () => {
     setIsActive(!isActive);
   };
   return (
-    // 추후 상품 등록자인지,일반 유저인지 id값을 비교하여
-    // 찜하기 버튼 or 수정&삭제 버튼을 표시하는 기능이 필요함.
     <>
       <div className="flex items-center">
         <HeartButton isButtonActive={isActive} stateHandler={activeHandler} />
