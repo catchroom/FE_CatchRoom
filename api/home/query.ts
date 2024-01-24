003fcdc6927dc29946cb904aa4d216bf50a79e02
jimin <moana16@naver.com>
2024-01-24 12:46:35 +0900

    ✨ feat : 메인 홈 캐치아이템 API 연결

--- api/home/query.ts
+++ api/home/query.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { postSaleProduct } from '../sale/api';
-import { getSalesHistory } from './api';
+import { getCatchItemsList, getSalesHistory } from './api';
 import { ProductItem } from '@/types/sale/type';
 //30
 export const useQueryGetSalesHistory = () => {
@@ -21,4 +21,12 @@ export const useMutaionPostSaleProduct = () => {
     mutationFn: (productItem: ProductItem) => postSaleProduct(productItem),
   });
   return mutation;
+};
+
+export const useQueryGetCatchItemsList = (dataType: number) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['getCatchItemsList'],
+    queryFn: () => getCatchItemsList(dataType),
+  });
+  return { data, isLoading };
 };
--- components/home/catch/catchSwiper/index.tsx
+++ components/home/catch/catchSwiper/index.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { ITEMS_INFO } from '@/constants/catchItems';
 import 'swiper/css';
 
 import CatchItem from '../catchItem';
+import { useQueryGetCatchItemsList } from '@/api/home/query';
+import { catchItems } from '@/types/common/catchItems/types';
 
 const CatchSwiper = () => {
+  const { data } = useQueryGetCatchItemsList(1);
+
+  const [slidesPerView, setSlidesPerView] = useState(1.1);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 410) {
+        setSlidesPerView(1.2);
+      } else {
+        setSlidesPerView(1.1);
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
-    <Swiper spaceBetween={12} slidesPerView={1.1} loop={true}>
-      {ITEMS_INFO.roomItems.map((item) => {
+    <Swiper spaceBetween={12} slidesPerView={slidesPerView} loop={true}>
+      {data?.list.map((item: catchItems) => {
         return (
-          <SwiperSlide key={item.id}>
+          <SwiperSlide key={item?.productId}>
             <CatchItem
-              key={item.id}
-              id={item.id}
-              roomName={item.roomName}
-              resDate={item.resDate}
-              oldPrice={item.oldPrice}
-              discount={item.discount}
-              location={item.location}
-              roomType="room"
+              key={item.productId}
+              productId={item.productId}
+              accommodationName={item.accommodationName}
+              checkIn={item.checkIn}
+              checkOut={item.checkOut}
+              originalPrice={item.originalPrice}
+              discountRate={item.discountRate}
+              sellPrice={item.sellPrice}
+              region={item.region}
+              image={item.image}
             />
           </SwiperSlide>
         );
