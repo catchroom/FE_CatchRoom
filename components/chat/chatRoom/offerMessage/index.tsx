21cf448c391006efacb72e090aa22c77a3229031
MinSeobKim <icdmiracle927@gmail.com>
2024-01-24 14:53:09 +0900

    ♻️ refactor : OfferMessage Refactoring, 불필요한 조건 삭제

--- components/chat/chatRoom/offerMessage/index.tsx
+++ components/chat/chatRoom/offerMessage/index.tsx
@@ -8,24 +8,24 @@ import { formatCurrency } from '@/utils/formatCurrency';
 
 const OfferMessage = ({ negoPrice, time, isSeller }: MessageProps) => {
   const router = useRouter();
-  if (isSeller) {
-    return (
-      <>
-        <p className="text-gray-500 text-t3">{formatTime(time)}</p>
-        <div className="w-full bg-white h-30 flex flex-col items-center border-[1px] border-gray-300 rounded-sm">
-          <Image
-            src="/productImage.png"
-            className="w-full h-24 object-cover"
-            alt="숙소사진"
-            width={60}
-            height={60}
-          />
-          <div className="float-right w-full p-4 ">
-            <p className="text-t2 font-semibold pb-2">가격을 제안했어요</p>
-            <p className="text-p2 text-gray-500">
-              요청 금액: {formatCurrency(negoPrice)}원
-            </p>
-          </div>
+  return (
+    <>
+      <p className="text-gray-500 text-t3">{formatTime(time)}</p>
+      <div className="w-full bg-white h-30 flex flex-col items-center border-[1px] border-gray-300 rounded-sm">
+        <Image
+          src="/productImage.png"
+          className="w-full h-24 object-cover"
+          alt="숙소사진"
+          width={60}
+          height={60}
+        />
+        <div className="float-right w-full p-4 ">
+          <p className="text-t2 font-semibold pb-2">가격을 제안했어요</p>
+          <p className="text-p2 text-gray-500">
+            요청 금액: {formatCurrency(negoPrice)}원
+          </p>
+        </div>
+        {isSeller && (
           <div className="flex w-full gap-1 px-4 pb-4">
             <DefaultBtn
               label="거절하기"
@@ -38,31 +38,10 @@ const OfferMessage = ({ negoPrice, time, isSeller }: MessageProps) => {
               onClick={() => router.push('/chat')}
             />
           </div>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <p className="text-gray-500 text-t3">{formatTime(time)}</p>
-        <div className="w-full bg-white h-30 flex flex-col items-center border-solid border border-gray-300 rounded-sm">
-          <Image
-            src="/productImage.png"
-            className="w-full h-24 object-cover"
-            alt="숙소사진"
-            width={60}
-            height={60}
-          />
-          <div className="float-right w-full p-5">
-            <p className="text-t2 font-semibold pb-2">가격을 제안했어요</p>
-            <p className="text-p2 text-gray-500">
-              제안 금액: {formatCurrency(negoPrice)}원
-            </p>
-          </div>
-        </div>
-      </>
-    );
-  }
+        )}
+      </div>
+    </>
+  );
 };
 
 export default OfferMessage;
