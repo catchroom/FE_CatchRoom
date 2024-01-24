ccb9e9f62c52eb2a96c21e7dd17d104c4ce492b0
junkue20 <junkue13@gmail.com>
2024-01-24 10:54:33 +0900

    ✨ feat : 상품 api 데이터 타입 추가

--- types/common/product/types.ts
+++ types/common/product/types.ts
@@ -7,3 +7,44 @@ export type ProductItem = {
   roomType: string;
   price: number;
 };
+
+// api 데이터 타입
+export type ApiDataType = {
+  code: number;
+  data: Data;
+};
+
+export type Data = {
+  seller_id: number;
+  accommodationName: string;
+  userIdentity: string;
+  chatRoomNumber: string;
+  accommodationUrl: AccommodationUrl[];
+  roomType: string;
+  roomUrl: RoomUrl[];
+  star: number;
+  originalPrice: number;
+  discountRate: number;
+  sellPrice: number;
+  checkIn: string;
+  checkOut: string;
+  introduction: string;
+  latitude: string;
+  longitude: string;
+  address: string;
+  roomNormalCapacity: number;
+  roomMaxCapacity: number;
+  accommodationService: string;
+  roomService: string;
+  totalRoomCapacity: number;
+};
+
+export type AccommodationUrl = {
+  id: number;
+  url: string;
+};
+
+export type RoomUrl = {
+  id: number;
+  url: string;
+};
