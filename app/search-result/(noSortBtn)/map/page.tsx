39cd0e8f6bdb529cb7860988a54e60fde403fa28
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-24 14:33:06 +0900

    ✨ feat: 필터링, noResults 추가

--- app/search-result/(noSortBtn)/map/page.tsx
+++ app/search-result/(noSortBtn)/map/page.tsx
@@ -1,15 +1,25 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Map from '@/components/search-result/map';
 import { formatDate } from '@/utils/formatDate';
 import { useAccommodationList } from '@/api/search-result/query';
 import { MarkerProps } from '@/types/search-result/map/type';
+import { useRecoilValue } from 'recoil';
+import {
+  regionIndex,
+  rangeDate,
+  roomIndex,
+  adultCountState,
+  childCountState,
+} from '@/atoms/search-detail/searchStates';
+import { format } from 'date-fns';
+import NoResults from '@/components/search-result/noResults';
 
 const Page = () => {
   const today = new Date();
   const tomorrow = new Date(today);
-  const [searchParams] = useState({
+  const [searchParams, setSearchParams] = useState({
     region: 'all',
     checkInStart: formatDate(today),
     checkInEnd: formatDate(tomorrow),
@@ -24,6 +34,48 @@ const Page = () => {
     isLoading,
     error,
   } = useAccommodationList(searchParams);
+
+  const selectedRegion = useRecoilValue(regionIndex);
+  const dateRange = useRecoilValue(rangeDate);
+  const selectedType = useRecoilValue(roomIndex);
+  const adultCount = useRecoilValue(adultCountState);
+  const childCount = useRecoilValue(childCountState);
+
+  //지역
+  useEffect(() => {
+    setSearchParams((prev) => ({
+      ...prev,
+      region: selectedRegion.join(','),
+    }));
+  }, [selectedRegion]);
+
+  //체크인 범위
+  useEffect(() => {
+    const startDate = dateRange?.from ?? new Date();
+    const endDate = dateRange?.to ?? new Date();
+
+    setSearchParams((prev) => ({
+      ...prev,
+      checkInStart: format(startDate, 'yyyy-MM-dd'),
+      checkInEnd: format(endDate, 'yyyy-MM-dd'),
+    }));
+  }, [dateRange]);
+
+  //숙박 유형
+  useEffect(() => {
+    setSearchParams((prev) => ({
+      ...prev,
+      type: selectedType.join(','),
+    }));
+  }, [selectedType]);
+
+  //인원 수
+  useEffect(() => {
+    setSearchParams((prev) => ({
+      ...prev,
+      pax: adultCount + childCount,
+    }));
+  }, [adultCount, childCount]);
 
   if (isLoading) {
     return <div>Loading</div>;
@@ -50,7 +102,11 @@ const Page = () => {
 
   return (
     <>
-      <Map markers={markersData} />
+      {accommodations && accommodations.length > 0 ? (
+        <Map markers={markersData} />
+      ) : (
+        <NoResults />
+      )}
     </>
   );
 };
