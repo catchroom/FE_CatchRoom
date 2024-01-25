a92e9c67fb2def529f588a335c832cc0eba83829
junkue20 <junkue13@gmail.com>
2024-01-25 15:13:13 +0900

    🐛 fix : 상품 상세 페이지 코드 일부 개선 및 Nav바 atom 초기화 함수 추가

--- components/roomInfo/body/roomInfo/itemCheckIn/index.tsx
+++ components/roomInfo/body/roomInfo/itemCheckIn/index.tsx
@@ -2,7 +2,7 @@
 import { useRoomItem } from '@/api/room-info/query';
 import CheckInDateComponent from '@/components/common/checkInDateComponent';
 import { UseParamsType } from '@/types/room-info/types';
-import { getDotDate } from '@/utils/get-dot-date';
+import { formatDateWithDay } from '@/utils/get-dot-date';
 import { useParams } from 'next/navigation';
 import React from 'react';
 
@@ -10,18 +10,16 @@ const ItemCheckInComponent = () => {
   const { id } = useParams() as UseParamsType;
   const { data, isLoading } = useRoomItem(id);
 
+  const checkInString = formatDateWithDay(data?.data.checkIn);
+  const checkOutString = formatDateWithDay(data?.data.checkOut);
   return (
     <>
       {isLoading ? (
         <div className="relative flex flex-wrap w-full h-[5.625rem] mt-2 items-center justify-around bg-gray-400 rounded-[4px] animate-pulse"></div>
       ) : (
         <CheckInDateComponent
-          CheckInDate={getDotDate(`${data && data.data.checkIn}`, false, false)}
-          CheckOutDate={getDotDate(
-            `${data && data.data.checkOut}`,
-            false,
-            true,
-          )}
+          CheckInDate={checkInString}
+          CheckOutDate={checkOutString}
         />
       )}
     </>
