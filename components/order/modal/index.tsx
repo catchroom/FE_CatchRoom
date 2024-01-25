d967be591c3ac9b2b20edf0cac16768c465744a8
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-25 19:20:47 +0900

    ✨ feat: 결제모달에 api 연결

--- components/order/modal/index.tsx
+++ components/order/modal/index.tsx
@@ -4,22 +4,32 @@ import { ModalProps } from '@/types/order/modal/type';
 import BookingDetails from './bookingDetails';
 import Disclaimer from './disclaimer';
 import Link from 'next/link';
+import { useQueryGetOrderProduct } from '@/api/order/query';
+import { useParams } from 'next/navigation';
 
 const PaymentModal = ({ isOpen, onClose }: ModalProps) => {
+  const params = useParams<{ id: string }>();
+  const productId = params ? parseInt(params.id, 10) : 0;
+
+  const { data, isLoading, error } = useQueryGetOrderProduct(productId);
+
   if (!isOpen) return null;
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading data</div>;
+  if (!data) return <div>No data available</div>;
+  const { accommodationName, productName, checkInDate, checkOutDate } =
+    data.product;
 
   return (
     <div className="fixed z-20 inset-0  bg-gray-1000 bg-opacity-50 flex justify-center items-center">
       <div className="relative bg-white w-full max-w-sm m-auto rounded">
         <div className="p-5">
           <BookingDetails
-            accommodationName="제주신라호텔"
-            roomName="스탠다드 더블 / 1박"
-            CheckInDate="12월 25일 (월)"
-            CheckOutDate="12월 26일 (화)"
-            normalCapacity={2}
-            maxCapacity={4}
-            imageUrl="room_image_url.jpg"
+            accommodationName={productName}
+            roomName={accommodationName}
+            CheckInDate={checkInDate}
+            CheckOutDate={checkOutDate}
           />
           <Disclaimer />
           <div className="flex justify-center space-x-4 mt-5">
