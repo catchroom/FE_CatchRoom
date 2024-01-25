5d83118a0534eaff79f222808edfee043b0a5a11
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-26 06:19:33 +0900

    ✨ feat: formatPhoneNumber에 옵셔널체이닝 추가

--- components/complete/navButton/index.tsx
+++ components/complete/navButton/index.tsx
@@ -1,10 +1,11 @@
 import { ActionButtonProps } from '@/types/order/modal/actionButton/type';
 import React from 'react';
 
-const NavButton = ({ label, colorClass }: ActionButtonProps) => {
+const NavButton = ({ label, colorClass, onClick }: ActionButtonProps) => {
   return (
     <button
       className={`w-full flex-grow min-w-[10rem] h-14 py-2.5 px-4 rounded text-center font-semibold ${colorClass}`}
+      onClick={onClick}
     >
       {label}
     </button>
--- utils/formatPhoneNumber.ts
+++ utils/formatPhoneNumber.ts
@@ -1,8 +1,8 @@
 export const formatPhoneNumber = (phoneNumber: string) => {
-  if (phoneNumber.length === 10) {
-    return phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
-  } else if (phoneNumber.length === 11) {
-    return phoneNumber.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3');
+  if (phoneNumber?.length === 10) {
+    return phoneNumber?.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+  } else if (phoneNumber?.length === 11) {
+    return phoneNumber?.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3');
   }
   return phoneNumber;
 };
