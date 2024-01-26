528b80aaf3b6d037bd88ef95170277c409a2a118
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-26 15:17:08 +0900

    🎨 style: productListControls z-20으로 변경

--- components/search-result/list/productListControls/index.tsx
+++ components/search-result/list/productListControls/index.tsx
@@ -26,7 +26,7 @@ const ProductListControls = () => {
   };
 
   return (
-    <div className="fixed z-10 flex items-center justify-between w-full max-w-[480px] p-5 mt-32 border-border-sub text-p1 font-semibold">
+    <div className="fixed z-20 flex items-center justify-between w-full max-w-[480px] p-5 mt-32 border-border-sub text-p1 font-semibold">
       <p className="text-t2 font-bold">총 {accommodationsCount}건</p>
       <div className="flex">
         <BottomSheetsWithoutCloseBtn
