dcacf8adc471be807240087153801fbbdc43e057
junkue20 <junkue13@gmail.com>
2024-01-26 18:43:08 +0900

    ✨ feat : 상세페이지 이미지 관련 에러처리

--- components/roomInfo/body/navbarSection/page/roomInfo/index.tsx
+++ components/roomInfo/body/navbarSection/page/roomInfo/index.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 
 const NavRoomInfoComponent = () => {
   const { id } = useParams() as UseParamsType;
-  const { data, isLoading } = useRoomItem(id);
+  const { data, isLoading, error } = useRoomItem(id);
 
   return (
     <>
@@ -18,7 +18,7 @@ const NavRoomInfoComponent = () => {
           <>
             <div className="max-w-[440px] h-full mb-2 bg-gray-400 rounded-[4px] animate-pulse" />
           </>
-        ) : (
+        ) : !error ? (
           <>
             {data?.data.roomUrl && (
               <Image
@@ -29,6 +29,21 @@ const NavRoomInfoComponent = () => {
                 className="rounded-md"
               />
             )}
+          </>
+        ) : (
+          <>
+            <div className="flex flex-shrink-0 items-center justify-center w-full h-full overflow-hidden rounded-md mr-4 bg-gray-300">
+              <svg
+                className="flex items-center justify-center text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="-15 0 50 18"
+                preserveAspectRatio="xMidYMid meet"
+              >
+                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+              </svg>
+            </div>
           </>
         )}
       </div>
