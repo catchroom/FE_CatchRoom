a7ad889ebe5bdd1d9df6cd32bb6a60e55b58035c
minseokiim <alstj991226@naver.com>
2024-01-27 14:17:19 +0900

    🐛 fix: 마이페이지 푸터 수정

--- components/mypage/banner/bannerFooter/index.tsx
+++ components/mypage/banner/bannerFooter/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Footer from '@/components/common/footer';
 
 const BannerFooter = () => {
   return (
     <div className="w-full flex flex-col items-start gap-1 bg-border px-3 py-4 text-p3 text-text-sub">
-      <h1 className=" text-t3 font-bold">(주) 캐치룸</h1>
-      <p>고객센터 02-4989-4989</p>
-      <p>평일 09:00 - 18:00 (점심시간 12:00 - 13:00)</p>
+      <Footer />
     </div>
   );
 };
