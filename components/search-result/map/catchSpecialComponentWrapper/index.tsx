ae7f363ae8cb962179bc7cb3c82b3d50345d8727
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-01-27 21:22:38 +0900

    ✨ feat: 지도 날짜 수정

--- components/search-result/map/catchSpecialComponentWrapper/index.tsx
+++ components/search-result/map/catchSpecialComponentWrapper/index.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import CatchSpecialComponent from '@/components/common/catchComponent';
 import { CatchSpecialComponentWrapperProps } from '@/types/search-result/map/type';
 import { useProductInfoPage } from '@/hooks/useProductInfoPage';
-import { formatToKoreanDate } from '@/utils/formatDate';
 
 const CatchSpecialComponentWrapper = ({
   selectedMarkerInfo,
 }: CatchSpecialComponentWrapperProps) => {
   const { pageHandler } = useProductInfoPage();
   if (!selectedMarkerInfo) return null;
-
-  const formattedCheckIn = formatToKoreanDate(selectedMarkerInfo.checkIn);
-  const formattedCheckOut = formatToKoreanDate(selectedMarkerInfo.checkOut);
-  const resDate = `${formattedCheckIn} - ${formattedCheckOut}`;
 
   return (
     <div className="absolute bottom-[11.5rem] left-1/2 transform -translate-x-1/2 w-9/12 z-[5] p-3 px-3 bg-white">
@@ -21,7 +16,8 @@ const CatchSpecialComponentWrapper = ({
         image={selectedMarkerInfo.image}
         accommodationName={selectedMarkerInfo.accommodationName}
         roomName={selectedMarkerInfo.roomName}
-        resDate={resDate}
+        checkIn={selectedMarkerInfo.checkIn}
+        checkOut={selectedMarkerInfo.checkOut}
         catchType={selectedMarkerInfo.catchType}
         originalPrice={selectedMarkerInfo.originalPrice}
         discountRate={selectedMarkerInfo.discountRate}
