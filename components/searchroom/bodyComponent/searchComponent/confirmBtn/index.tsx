317601b995b1e90fd6b7da8384be4d9caf0ab2b9
junkue20 <junkue13@gmail.com>
2024-01-27 22:15:14 +0900

    🐛 fix : 검색하기 버튼 위치 fixed

--- components/searchroom/bodyComponent/searchComponent/confirmBtn/index.tsx
+++ components/searchroom/bodyComponent/searchComponent/confirmBtn/index.tsx
@@ -7,15 +7,17 @@ const ConfirmBtnComponent = () => {
   const { isBtnLoading, btnHandler } = useBtnLoading('/search-result/list');
 
   return (
-    <Button
-      placeholder="button"
-      loading={isBtnLoading ? true : false}
-      type="button"
-      onClick={btnHandler}
-      className="font-pretendard flex items-center justify-center rounded-[4px] h-11 bg-action-primary text-t2 text-white font-semibold shadow-none"
-    >
-      {isBtnLoading ? '검색중...' : '검색하기'}
-    </Button>
+    <div className="fixed bottom-0 w-full px-5 mb-5 ">
+      <Button
+        placeholder="button"
+        loading={isBtnLoading ? true : false}
+        type="button"
+        onClick={btnHandler}
+        className="font-pretendard flex items-center justify-center rounded-[4px] w-full h-11 bg-action-primary text-t2 text-white font-semibold shadow-none"
+      >
+        {isBtnLoading ? '검색중...' : '검색하기'}
+      </Button>
+    </div>
   );
 };
 
